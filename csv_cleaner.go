fcbf09ab630fac98621788ed78ae7577269c6a29
Christopher Seven Phiri <chris7phiri@gmail.com>
2021-05-12 12:48:59 +0200

    parsing intergers

--- csv_cleaner.go
+++ csv_cleaner.go
@@ -7,28 +7,29 @@ import (
 	"fmt"
 	"log"
 	"os"
+	"strconv"
 	"strings"
 	"time"
 )
 
 type DailyCompanyRate struct {
-	NO         string `json:"no"`
-	HIGH       string `json:"high"`
-	LOW        string `json:"low"`
-	CODE       string `json:"code"`
-	BUY        string `json:"buy"`
-	SELL       string `json:"sell"`
-	PCP        string `json:"pcp"`
-	TCP        string `json:"tcp"`
-	VOL        string `json:"vol"`
-	DIVNET     string `json:"div_net"`
-	DIVYIELD   string `json:"div_yield"`
-	EEARNYIELD string `json:"earn_yield"`
-	PERATIO    string `json:"pe_ratio"`
-	PBVRATION  string `json:"pbv_ratio"`
-	CAP        string `json:"cap"`
-	PROFIT     string `json:"profit"`
-	SHARES     string `json:"shares"`
+	NO         string  `json:"no"`
+	HIGH       string  `json:"high"`
+	LOW        string  `json:"low"`
+	CODE       string  `json:"code"`
+	BUY        float64 `json:"buy"`
+	SELL       float64 `json:"sell"`
+	PCP        float64 `json:"pcp"`
+	TCP        float64 `json:"tcp"`
+	VOL        float64 `json:"vol"`
+	DIVNET     float64 `json:"div_net"`
+	DIVYIELD   float64 `json:"div_yield"`
+	EEARNYIELD float64 `json:"earn_yield"`
+	PERATIO    float64 `json:"pe_ratio"`
+	PBVRATION  float64 `json:"pbv_ratio"`
+	CAP        float64 `json:"cap"`
+	PROFIT     float64 `json:"profit"`
+	SHARES     float64 `json:"shares"`
 }
 
 func Clean(csvFile string, errorPath string) ([]DailyCompanyRate, []string) {
@@ -71,30 +72,29 @@ func Clean(csvFile string, errorPath string) ([]DailyCompanyRate, []string) {
 	var rate DailyCompanyRate
 	for i, word := range records {
 		if len(word) == 17 {
-
-			rate.NO = strings.TrimSpace(word[0])
-			rate.HIGH = strings.TrimSpace(word[1])
-			rate.LOW = strings.TrimSpace(word[2])
-			rate.CODE = strings.TrimSpace(word[3])
-			rate.BUY = strings.TrimSpace(word[4])
-			rate.SELL = strings.TrimSpace(word[5])
-			rate.PCP = strings.TrimSpace(word[6])
-			rate.TCP = strings.TrimSpace(word[7])
-			rate.VOL = strings.TrimSpace(word[8])
-			rate.DIVNET = strings.TrimSpace(word[9])
-			rate.DIVYIELD = strings.TrimSpace(word[10])
-			rate.EEARNYIELD = strings.TrimSpace(word[11])
-			rate.PERATIO = strings.TrimSpace(word[12])
-			rate.PBVRATION = strings.TrimSpace(word[13])
-			rate.CAP = strings.TrimSpace(word[14])
-			rate.PROFIT = strings.TrimSpace(word[15])
-			rate.SHARES = strings.TrimSpace(word[16])
-
-			if Verify(rate) {
+			if Verify(word) {
+				rate.NO = strings.TrimSpace(word[0])
+				rate.HIGH = strings.TrimSpace(word[1])
+				rate.LOW = strings.TrimSpace(word[2])
+				rate.CODE = strings.TrimSpace(word[3])
+				rate.BUY = parseFloat(strings.TrimSpace(word[4]))
+				rate.SELL = parseFloat(strings.TrimSpace(word[5]))
+				rate.PCP = parseFloat(strings.TrimSpace(word[6]))
+				rate.TCP = parseFloat(strings.TrimSpace(word[7]))
+				rate.VOL = parseFloat(strings.TrimSpace(word[8]))
+				rate.DIVNET = parseFloat(strings.TrimSpace(word[9]))
+				rate.DIVYIELD = parseFloat(strings.TrimSpace(word[10]))
+				rate.EEARNYIELD = parseFloat(strings.TrimSpace(word[11]))
+				rate.PERATIO = parseFloat(strings.TrimSpace(word[12]))
+				rate.PBVRATION = parseFloat(strings.TrimSpace(word[13]))
+				rate.CAP = parseFloat(strings.TrimSpace(word[14]))
+				rate.PROFIT = parseFloat(strings.TrimSpace(word[15]))
+				rate.SHARES = parseFloat(strings.TrimSpace(word[16]))
 				rates = append(rates, rate)
 			} else {
 				errors = append(errors, fmt.Sprintf("line: %b values: %s", i, word))
 			}
+
 		}
 	}
 
@@ -119,17 +119,35 @@ func logErrors(errors []string, path string) {
 	}
 }
 
-func Verify(rate DailyCompanyRate) bool {
-	if isNotEmpty(rate.CODE) && isNotEmpty(rate.BUY) && isNotEmpty(rate.SELL) && isNotEmpty(rate.PCP) &&
-		isNotEmpty(rate.TCP) && isNotEmpty(rate.VOL) && isNotEmpty(rate.DIVNET) && isNotEmpty(rate.DIVYIELD) &&
-		isNotEmpty(rate.EEARNYIELD) && isNotEmpty(rate.PERATIO) && isNotEmpty(rate.PBVRATION) && isNotEmpty(rate.CAP) &&
-		isNotEmpty(rate.PROFIT) && isNotEmpty(rate.SHARES) {
-		return true
+func parseFloat(value string) float64 {
+
+	if strings.Contains(value, "(") {
+		raw := strings.ReplaceAll(value, "(", "")
+		raw = strings.ReplaceAll(raw, ")", "")
+
+		raw = strings.TrimSpace(raw)
+		value = fmt.Sprint("-", raw)
 	}
 
-	return false
+	f, err := strconv.ParseFloat(value, 64)
+	if err != nil {
+
+		return 0
+	}
+	return f
 }
 
-func isNotEmpty(value string) bool {
-	return len(value) > 0
+func Verify(rate []string) bool {
+	for i, value := range rate {
+		if i > 2 {
+			if isEmpty(value) {
+				return false
+			}
+		}
+	}
+	return true
+}
+
+func isEmpty(value string) bool {
+	return len(value) <= 0
 }
